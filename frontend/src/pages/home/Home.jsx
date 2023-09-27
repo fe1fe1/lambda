fff6b4783cfa41b3b341cd4a8b6df352082fcdc4
Franco Macias <francogm98@hotmail.com>
2023-09-27 00:49:36 -0300

    home page snippets

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,3 +1,9 @@
+import {
+    faCreditCard,
+    faGamepad,
+    faTruck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import HomeCarousel from "../../components/carousel/Carousel";
 import "./Home.scss";
@@ -14,10 +20,42 @@ const Home = () => {
                 <div className="carousel w-50 mx-auto">
                     <HomeCarousel />
                 </div>
-                <div className="block-container cards-section">
-                    test 
-                </div>
+                <div className="snippets">
+                    <div className="product-snippet">
+                        <FontAwesomeIcon
+                            className="snippet-icon"
+                            icon={faGamepad}
+                            style={{ color: "#000000" }}
+                            size="10x"
+                        />
 
+                        <p className="snippet-caption">
+                            We have the best products on the market.
+                        </p>
+                    </div>
+                    <div className="shipping-snippet">
+                        <FontAwesomeIcon
+                            className="snippet-icon"
+                            icon={faTruck}
+                            style={{ color: "#000000" }}
+                            size="10x"
+                        />
+                        <p>
+                            We do fast delivery, without cost under $1000
+                            purchase.
+                        </p>
+                    </div>
+                    <div className="payment-snippet">
+                        <FontAwesomeIcon
+                            className="snippet-icon"
+                            icon={faCreditCard}
+                            style={{ color: "#000000" }}
+                            size="10x"
+                        />
+                        <p>We provide a wide variety of payment methods.</p>
+                    </div>
+                </div>
+                <div className="block-container show-me-section">test</div>
             </div>
         </div>
     );
